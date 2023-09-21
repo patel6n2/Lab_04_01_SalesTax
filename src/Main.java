f552db892ecbeefa546796a8b291c8aabcfe7daf
Nishaan Patel <patel6n2@mail.uc.edu>
2023-09-20 23:42:22 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        double itemPrice = 5;
+        double salesTax = 0;
+        salesTax = itemPrice * 0.05;
+        System.out.println("The sales tax on a purchase of " + itemPrice + " is " + salesTax);
+    }
+}
